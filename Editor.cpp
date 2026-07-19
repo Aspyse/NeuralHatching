2c59752943873bd7e0e9f8b089d16c9a42bf1f1b
Aspyse <derek.burias@gmail.com>
2026-07-20 04:11:46 +0800

    hotfix screen default width

--- Editor.cpp
+++ Editor.cpp
@@ -7,7 +7,7 @@ bool Editor::Initialize()
 {
 	// Window init
 	// fits a 1024x1024 (2x2 of 512) grid plus side panels
-	const int SCREEN_WIDTH = 1536,
+	const int SCREEN_WIDTH = 1920,
 		SCREEN_HEIGHT = 1024;
 	const float NEAR_PLANE = 0.1f,
 		FAR_PLANE = 6.0f;
